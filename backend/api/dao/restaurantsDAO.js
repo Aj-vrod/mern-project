94ae93021b92951c8d4350a71b9fd7bafb54c6e5
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-06-05 15:18:33 +0200

    created RestaurantsDAO class

---
+++ backend/api/dao/restaurantsDAO.js
@@ -0,0 +1,15 @@
+let restaurants
+
+export default class RestaurantsDAO {
+    static async injectDB(conn) {
+        if (restaurants) {
+            return
+        }
+        try {
+            restaurants = await conn.db(process.env.RESTREV_NS).collection("restaurants")
+        } catch (e) {
+            console.error(`Unable to establish a connection handle in restaurantsDAO: ${e}`)
+        }
+
+    } 
+}
