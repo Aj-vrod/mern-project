04f2b407a7097ab1667e97cf99cf0911a214b1bc
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-06-07 00:21:01 +0200

    Dad

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -2,4 +2,15 @@ import mongodb from "mongodb"
 const ObjectId = mongodb.ObjectID
 
 let reviews
-
+export default class ReviewsDAO {
+    static async injectDB(conn) {
+        if (reviews) {
+            return
+        }
+        try {
+            reviews = await conn.db(process.env.RESTREV_NS).collection("reviews")
+        } catch(e) {
+            console.error(`Unable to establish collection handles in userDAO: ${e}`)
+        }
+    }
+}
