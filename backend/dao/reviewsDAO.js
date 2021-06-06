d246b0895f2c5dc7d676f4bf8b6073bd4774eb41
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-06-07 00:26:02 +0200

    added addreview function

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -13,4 +13,22 @@ export default class ReviewsDAO {
             console.error(`Unable to establish collection handles in userDAO: ${e}`)
         }
     }
+
+    static async addReview(restaurantId, user, review, date) {
+        try {
+            const reviewDoc = {
+                name: user.name,
+                userId: user._id,
+                date: date,
+                text: review,
+                restaurant_id: ObjectId(restaurantId),
+            }
+            return await reviews.insertOne(reviewDoc)
+        } catch(e) {
+            console.error(
+                `Unable to post review: ${e}`
+            )
+            return { error: e }
+        }
+    }
 }
