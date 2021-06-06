7f9dcb83ed08a884d1e79adddb892f8651a5d3ce
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-06-06 23:47:15 +0200

    added review route

---
+++ backend/api/reviews.controller.js
@@ -0,0 +1,9 @@
+import ReviewsDAO from "../dao/reviewsDAO.js"
+
+export default class ReviewsController {
+    static async apiPostReview(req, res, next) {
+        try {
+            
+        }
+    }
+}
