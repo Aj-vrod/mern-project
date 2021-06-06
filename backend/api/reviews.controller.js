f8f7b1b13779a7790f466dbd1822cc6e1aaf9936
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-06-07 00:08:30 +0200

    finished apiupdatereview function

--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -35,6 +35,20 @@ export default class ReviewsController {
                 text,
                 date,
             )
+
+            var { error } = reviewResponse
+            if (error) {
+                res.status(400).json({ error })
+            }
+            if (reviewResponse.modifiedCount === 0) {
+                throw new Error(
+                    "unable to update review - user may not be original poster",
+                )
+            }
+
+            res.json({ status: "success" })
+        } catch(e) {
+            res.status(500).json({ error: e.message })
         }
     }
 }
