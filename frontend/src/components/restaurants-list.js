d2bb6c47c521e9196eb8d8e4fd33c66ccf738b5a
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-06-07 15:06:12 +0200

    added functions to RestaurantsList

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -14,6 +14,33 @@ const RestaurantsList = (props) => {
     retrieveCuisines();
   }, [])
 
+  const onChangeSearchName = e => {
+    const searchName = e.target.value;
+    setSearchName(searchName);
+  }
+
+  const onChangeSearchZip = e => {
+    const searchZip = e.target.value;
+    setSearchZip(searchZip);
+  }
+
+  const onChangeSearchCuisine = e => {
+    const searchCuisine = e.target.value;
+    setSearchCuisine(searchCuisine);
+  }
+
+  const retrieveRestaurants = () => {
+    RestaurantDataService.getAll()
+      .then(response => {
+        console.log(response.data);
+        setRestaurants(response.data.restaurants);
+      })
+      .catch(e => {
+        console.log(e);
+      })
+  }
+
+
   return (
     <div className="App">
       Hello World
