9991829cf755c039d083ea8544e2516f72c0fc5a
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-06-07 16:10:01 +0200

    added all functions for Restaurant class

--- frontend/src/components/restaurants.js
+++ frontend/src/components/restaurants.js
@@ -3,11 +3,48 @@ import RestaurantDataService from "../services/restaurant";
 import { Link } from "react-router-dom";
 
 const Restaurant = props => {
+  const initialRestaurantState = {
+    id: null,
+    name: "",
+    address: {},
+    cuisine: "",
+    reviews: [],
+  };
+  const [restaurant, setRestaurant] = useState(initialRestaurantState);
+  
+  const getRestaurant = id => {
+    RestaurantDataService.get(id)
+      .then(response => {
+        setRestaurant(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  useEffect(() => {
+    getRestaurant(props.match.params.id);
+  }, [props.match.params.id]);
+
+  const deleteReview = (reviewId, index) => {
+    RestaurantDataService.deleteReview(reviewId)
+      .then(response => {
+        setRestaurant((prevState) => {
+          prevState.reviews.splice(index, 1)
+          return ({
+            ...prevState
+          })
+        })
+      })
+      .catch(e => {
+        console.log(e);
+      })
+  }
+
   return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    
+  )
 }
 
 export default Restaurant;
