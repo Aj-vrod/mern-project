14d8c40fc0eeca1331cba6dfb2200f15f8a590c0
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-06-07 12:14:27 +0200

    implementing getRestaurantById function

--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -1,3 +1,5 @@
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectID
 let restaurants
 
 export default class RestaurantsDAO {
@@ -51,6 +53,46 @@ export default class RestaurantsDAO {
                 `Unable to convert cursor to array or problem counting documents, ${e}`
             )
             return { restaurantsList: [], totalNumRestaurants: 0 }
+        }
+    }
+
+    static async getRestaurantById(id) {
+        try {
+            const pipeline = [
+                {
+                    $match: {
+                        _id: new ObjectId(id),
+                    },
+                },
+                {
+                    $lookup: {
+                        from: "reviews",
+                        let: {
+                            id: "$_id",
+                        },
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ["restaurant_id", "$$id"],
+                                    },
+                                },
+                            },
+                            {
+                                $sort: {
+                                    date: -1,
+                                },
+                            },
+                        ],
+                        as: "reviews",
+                    },
+                },
+                {
+                    $addFields: {
+                        reviews: "$reviews",
+                    },
+                },
+            ]
         }
     }
 }
