28301e31dd45a84a376a7fbaab8a0bf8e713b873
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-06-07 13:46:05 +0200

    added container div in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,16 @@ import RestaurantsList from "./components/restaurants-list";
 import Login from "./components/login";
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
   return (
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
@@ -21,19 +31,43 @@ function App() {
             </Link>
           </li>
           <li className="nav-item">
-            { user ? (
-              <a onClick={logout} className="nav-link" style={{cursor: 'pointer'}}>
+            {user ? (
+              <a
+                onClick={logout}
+                className="nav-link"
+                style={{ cursor: "pointer" }}
+              >
                 Logout {user.name}
               </a>
-              ) : (
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              )
-            }
+            ) : (
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            )}
           </li>
         </div>
       </nav>
+
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/", "/restaurants"]} component={RestaurantsList} />
+          <Route 
+            path="/restaurants/:id/review" 
+            render={(props) => (
+              <AddReview {...props} user={user} />
+            )} />
+          <Route 
+            path="/restaurants/:id" 
+            render={(props) => (
+              <Restaurant {...props} user={user} />
+            )} />
+          <Route 
+            path="/login" 
+            render={(props) => (
+              <Login {...props} login={login} />
+            )} />
+        </Switch>
+      </div>
     </div>
   );
 }
