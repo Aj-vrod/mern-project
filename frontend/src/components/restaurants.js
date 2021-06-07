e670f5b50f71bb64a729419bdd672074a35425f5
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-06-07 16:01:06 +0200

    updating restaurants.js

--- frontend/src/components/restaurants.js
+++ frontend/src/components/restaurants.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import RestaurantDataService from "../services/restaurant";
+import { Link } from "react-router-dom";
 
-function Restaurant() {
+const Restaurant = props => {
   return (
     <div className="App">
       Hello World
